da78ec898db3e96f04ce882b1b808fa4788610f0
Harasztia Peter <hpeter1988@msn.com>
2019-04-04 16:13:28 +0200

    Def basic info Task

---
+++ week-01/day-3/define-basic-info.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+let myName: string = "Harasztia Péter"
+let myAge: number = 30
+let myHight: number = 1.7
+let married: boolean = false
+
+console.log(myName)
+console.log(myAge)
+console.log(myHight)
+console.log(married)
+
