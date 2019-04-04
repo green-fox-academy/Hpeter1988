c50c26ccb80a5050b1c1aa9cbaaf05d90d3eb145
Harasztia Peter <hpeter1988@msn.com>
2019-04-04 15:37:13 +0200

    hours task

---
+++ week-01/day-3/hours.ts
@@ -0,0 +1,15 @@
+'use strict';
+let hoursDaily: number = 6
+let days: number = 5
+let weeks: number = 17
+// An average Green Fox attendee codes 6 hours daily
+// The semester is 17 weeks long
+//
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+console.log(hoursDaily * days * weeks);
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+console.log(hoursDaily * days / 52 * 100);
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
