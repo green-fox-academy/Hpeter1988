c6bb712523f1ff63adb736972b5d2532e52cadf5
Harasztia Peter <hpeter1988@msn.com>
2019-04-05 11:20:31 +0200

    i-wont-cheat-on-the-exams task

---
+++ week-01/day-4/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+let a: number = 1;
+while (a <= 100) {
+  console.log(a + " I won't cheat on the exam!"); 
+  a++;
+}
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
