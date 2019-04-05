87f4f7bcbbd448a9c79b8fd2baf7700ec5bf7426
Harasztia Peter <hpeter1988@msn.com>
2019-04-05 12:03:32 +0200

    multiplication-table task

---
+++ week-01/day-4/multiplication-table.ts
@@ -0,0 +1,25 @@
+    
+let a: number = 15;
+let b: number = 1;
+
+while (a <= 150 && b<=10){
+  console.log(b + " * 15 = " + a * b);
+  b++;
+}
+
+// Create a program that
+// prints the multiplication table with number
+//
+// Example:
+// The number 15 should print:
+//
+// 1 * 15 = 15
+// 2 * 15 = 30
+// 3 * 15 = 45
+// 4 * 15 = 60
+// 5 * 15 = 75
+// 6 * 15 = 90
+// 7 * 15 = 105
+// 8 * 15 = 120
+// 9 * 15 = 135
+// 10 * 15 = 150
