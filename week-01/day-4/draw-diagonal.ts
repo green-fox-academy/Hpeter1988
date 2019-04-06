b33b8e55e5157105e911138f0749be25dc357ffb
Harasztia Peter <hpeter1988@msn.com>
2019-04-06 21:52:39 +0200

    draw-diagonal task

--- week-01/day-4/draw-diagonal.ts
+++ week-01/day-4/draw-diagonal.ts
@@ -1,15 +1,8 @@
+
 'use strict';
 
 let lineCount: number = 6;
 
-// Write a program that draws a square like this:
-//
-// %%%%%%
-// %    %
-// %    %
-// %    %
-// %    %
-// %%%%%%
 for(let i: number = 1; i <= lineCount; i++){
     let doboz: string = ''
     for(let j: number = 1; j<= lineCount; j++){
