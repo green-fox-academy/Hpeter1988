f3c26f28ab8fff9a721b0884fea9149e45969a57
Harasztia Peter <hpeter1988@msn.com>
2019-04-08 13:52:03 +0200

    greet.ts added

---
+++ week-02/day-1/greet.ts
@@ -0,0 +1,15 @@
+
+    
+// -  Create variable named `nameToGreet` and assign the value `Greenfox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Greenfox`
+//     -  Prepare for the special case when no parameters are given
+// -  Greet `nameToGreet`
+
+let nameToGreat: string = 'Greenfox'
+
+function greet(greets: string):string{
+    return greets = "Greetings, dear " + greets
+}
+
+console.log(greet(nameToGreat))
