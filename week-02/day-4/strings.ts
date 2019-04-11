367d072c8230a8dfa3a5999bc88d19d76c380a80
Harasztia Peter <hpeter1988@msn.com>
2019-04-11 13:10:14 +0200

    week-02/day-4: strings.ts added and bunnies-again.ts feat(earcount funct written)

---
+++ week-02/day-4/strings.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+//Given a string, compute recursively (no loops) a new string where all the lowercase 'x' 
+//chars have been changed to 'y' chars.
+
+
+function stringer(main: string,) :string{
+    
+    let xArr: number =  main.indexOf('x')
+    console.log(xArr)
+    }
+    
+    console.log(stringer(2, 3))
+    
