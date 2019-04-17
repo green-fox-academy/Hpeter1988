6e68ad3b2ad03d2c621c1e2a71388cc668604119
Harasztia Peter <hpeter1988@msn.com>
2019-04-17 13:58:48 +0200

    week-03/day-2/sharpie.ts added  feat(class with use func added)

---
+++ week-03/day-3/sharpie.ts
@@ -0,0 +1,29 @@
+'use strict'
+
+//Create Sharpie class
+//We should know about each sharpie their color (which should be a string), width (which will be a floating point number), inkAmount (another floating point number)
+//When creating one, we need to specify the color and the width
+//Every sharpie is created with a default 100 as inkAmount
+//We can use() the sharpie objects
+//which decreases inkAmount
+
+class Sharpie {
+  color: string;
+  width: number;
+  inkAmount: number;
+
+
+  constructor(color: string, width: number,) {
+  
+    this.color = color;
+    this.width = width;
+    this.inkAmount = 100;
+  
+  }
+use(){this.inkAmount --}
+}
+
+let myFilc: Sharpie = new Sharpie ('red',0.5 );
+console.log(myFilc);
+myFilc.use();
+console.log(myFilc);
