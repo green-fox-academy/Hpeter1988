5e289773102f2bf8a9f898e698f2a4ce71f12fa3
Harasztia Peter <hpeter1988@msn.com>
2019-04-17 13:43:04 +0200

    week-03/day-2/animals.ts added  feat(class animals created)

---
+++ week-03/day-3/animals.ts
@@ -0,0 +1,39 @@
+'use strict'
+
+//Create an Animal class
+//Every animal has a hunger value, which is a whole number
+//Every animal has a thirst value, which is a whole number
+//when creating a new animal object these values are created with the default 50 value
+//Every animal can eat() which decreases their hunger by one
+//Every animal can drink() which decreases their thirst by one
+//Every animal can play() which increases both by one
+
+class Animals {
+  animalName: string;
+  hunger: number;
+  thirst: number;
+
+
+  constructor(animalName: string, ) {
+
+    this.animalName = animalName;
+    this.hunger = 50;
+    this.thirst = 50;
+  }
+  drink() { this.thirst++ }
+
+  eat() { this.hunger++ }
+
+  play() {
+  this.hunger--;
+    this.thirst--
+  }
+
+}
+
+const animal = new Animals('Crow')
+console.log(animal);
+animal.play();
+console.log(animal);
+animal.eat();
+console.log(animal);
