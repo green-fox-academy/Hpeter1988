ab2d29634178ca81b388d06f52bf1b7c021d2101
Harasztia Peter <hpeter1988@msn.com>
2019-04-18 13:41:08 +0200

    week-03/day-3/domino.ts dominoes.ts added feat(dominoes ordered) with  help

---
+++ week-03/day-3/dominoes.ts
@@ -0,0 +1,47 @@
+'use strict'
+
+import { Domino } from "./domino";
+
+function initializeDominoes(): Domino[] {
+  let dominoes = [];
+  dominoes.push(new Domino(5, 2));
+  dominoes.push(new Domino(4, 6));
+  dominoes.push(new Domino(1, 5));
+  dominoes.push(new Domino(6, 7));
+  dominoes.push(new Domino(2, 4));
+  dominoes.push(new Domino(7, 1));
+  return dominoes;
+}
+
+function print(dominoes: Domino[]) {
+  dominoes.forEach(function (value) {
+    console.log(value);
+  });
+}
+
+let dominoes = initializeDominoes();
+/** You have the list of Dominoes */
+/** Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides */
+/** eg: [2, 4], [4, 3], [3, 5] ... */
+
+print(dominoes);
+
+
+function print2(dominoes: Domino[]) {
+  dominoes.forEach(function (value) {
+    console.log(value);
+  });
+}
+
+let snake = []
+snake.push(dominoes[0])
+for (let i: number = 1; i <= dominoes.length - 1; i++) {
+  for (let j: number = 0; j <= dominoes.length -1; j++) {
+    if (dominoes[j].values[0] === snake[i-1].values[1]){
+      snake.push(dominoes[j])
+    }
+
+  }
+
+}
+console.log(snake)
