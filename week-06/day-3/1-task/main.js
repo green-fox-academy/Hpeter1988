2ca3e07e61df0583bbe525f1b490f540b3a5dc5e
Harasztia Peter <hpeter1988@msn.com>
2019-05-15 12:18:33 +0200

    task 1 completed

--- week-06/day-3/1-task/main.js
+++ week-06/day-3/1-task/main.js
@@ -1,4 +1,7 @@
 'use strict'
-
-let lastLine = document.querySelector('.animal')
-lastline.textContent
+let myParagraphs = document.getElementsByTagName('p')
+let lastLine = document.getElementsByClassName('animals')[0].innerHTML
+console.log(lastLine)
+for (let i = 0; i < myParagraphs.length; i++){
+myParagraphs[i].innerHTML = lastLine
+}
