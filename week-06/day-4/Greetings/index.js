9219bb082a656e4a17921b202d8182ec2ed7d51e
Harasztia Peter <hpeter1988@msn.com>
2019-05-16 13:46:14 +0200

    Greatings app completed

---
+++ week-06/day-4/Greetings/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// home page
+app.get('/', (req, res) => {
+
+  let name = req.query.name;
+  if(!name){
+    name = 'Guest';
+  }
+  res.render('home',{title: name});
+});
+
+
+
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
