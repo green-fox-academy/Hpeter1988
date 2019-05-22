4cb3fc87d7ad23c3ae729805582863d1d862829a
Harasztia Peter <hpeter1988@msn.com>
2019-05-22 13:23:00 +0200

    Greeting endpoint created

--- week-07/day-3/frontend/app.js
+++ week-07/day-3/frontend/app.js
@@ -21,6 +21,23 @@ app.get('/doubling', (req, res) => {
   res.send(answer)
 });
 
+app.get('/greeter', (req, res) => {
+  let inputName = req.query.name;
+  let inputTitle = req.query.title;
+  let answer = {}
+  if(inputName && inputTitle){
+  answer.welcome_message = `Oh, hi there ${inputName}, my dear ${inputTitle}!`}
+  else if (inputName && !inputTitle){
+    answer.error = "Please provide a title!"
+  }
+  else if (!inputName && inputTitle){
+    answer.error = "Please provide a name!"
+  }
+  else if (!inputName && !inputTitle) {
+    answer.error = 'Please provide a name and title'}
+  res.send(answer)
+});
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
