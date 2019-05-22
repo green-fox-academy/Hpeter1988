bc7804658ee4c23a2a8e77b815a54ed15cfe71c9
Harasztia Peter <hpeter1988@msn.com>
2019-05-22 12:03:55 +0200

    doubling endpoint created

--- week-07/day-3/frontend/app.js
+++ week-07/day-3/frontend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(express.static('assets'))
 
@@ -9,7 +9,18 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/doubling', (req, res) => {
+  let input = req.query.input;
+  let answer = {};
+  if(input){
+  anwser.received = input*1;
+  anwser.result = input*2;
+  }
+  else anwser.error = 'Please provide an input!'
 
+  res.send(answer)
+  
+});
 
 
 app.listen(PORT, () => {
