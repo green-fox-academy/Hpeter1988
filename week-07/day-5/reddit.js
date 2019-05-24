8477475feffb18733c518616443facd2ee6d49d6
Harasztia Peter <hpeter1988@msn.com>
2019-05-24 11:29:58 +0200

    SQL databse and server connected

--- week-07/day-5/reddit.js
+++ week-07/day-5/reddit.js
@@ -3,6 +3,25 @@ const app = express();
 const table = 'posts';
 const PORT = 3000;
 app.use(express.json());
+const mysql = require('mysql');
+require('dotenv').config();
+
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+conn.connect(err => {
+  if (err) {
+    console.log(err.toString());
+    return;
+  }
+  console.log('connection to DB is OK ✨');
+});
+
+
+
 
 app.get('/', (req, res) => {
   res.send("WHAT'S UP MATE?");
